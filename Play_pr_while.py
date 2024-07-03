6d94e795350e63ebf69db8262a47fbd15b45484f
OlegKo2024 <4oki66@gmail.com>
2024-07-04 02:28:32 +0300

    Add files via upload

--- Play_pr_while.py
+++ Play_pr_while.py
@@ -63,18 +63,16 @@
 # 0 99 Я тут
 # Process finished with exit code 0
 
-# my_list = [0, 99, -5, 9]
+# my_list = [1, 99, -5, 9]
 # index = 0
 # while index < len(my_list):
 #     if my_list[index] >= 0:
 #         print(my_list[index], end=' ')
 #         index += 1
-#         continue
 #     elif my_list[index] < 0:
 #         index += 1
-#         continue
 # print('Конец списка')
-# 0 99 9 Конец списка
+# 1 99 9 Конец списка, continue нигде не надо, если условие не выполнено, то программа идет на следующий уровень
 # Process finished with exit code 0
 
 # my_list = [0, -99, 5, 9]
@@ -100,12 +98,47 @@
 # 0 99 5 9 Game is over, выполнено условие, но после +1, закончился список и программе перешла к строке 'Game is over'
 # Process finished with exit code 0
 
-my_list = [0, 99, 5, 9]
+# my_list = [0, 99, 5, 9]
+# index = 0
+# while index < len(my_list):
+#     if my_list[index] >= 0:
+#         print(my_list[index], end=' ')
+#         index += 1
+#     else:
+#         break
+# print('Game is over') # или минус или конец списка
+
+# my_list = [0, 99, -5, 9]
+# index = 0
+# while index < len(my_list):
+#     if my_list[index] >= 0:
+#         print(my_list[index], end=' ')
+#         index += 1
+#     else:
+#         index += 1
+#         continue
+# print('Game is over') # только положительные числа
+
+my_list = [0, 99, -5, 9]
 index = 0
 while index < len(my_list):
     if my_list[index] >= 0:
         print(my_list[index], end=' ')
         index += 1
-    else:
-        break
-print('Game is over')
+        continue
+    index += 1
+    continue
+print('Game is over') # только положительные числа, но без else
+
+# a = int(input('a:'))
+# b = int(input('b:'))
+# x = a
+# while x != b:
+#     if x % 5 == 0 and x % 7 == 0:
+#         print(x, end=' ')
+#     x = x + 1
+
+# если совпадает, то идет далее, ниже по уровню (по ветви), если не совпадает, то выходит за уровень (ветвь)
+# Например, в примере ниже, перебирание начинает с единицы, оно не соответствент условию, соответственно,
+# идет на х = х + 1 - прибавление следующего значения и цикл повторяется.
+
