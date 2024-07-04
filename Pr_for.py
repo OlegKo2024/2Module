95e073b4134fcb638eccd06be94a87e2b74180a9
OlegKo2024 <4oki66@gmail.com>
2024-07-04 12:29:47 +0300

    Add files via upload

--- Pr_for.py
+++ Pr_for.py
@@ -16,11 +16,10 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 prime = []
 not_prime = []
 for i in numbers:
-    # print(i, end=' ') # 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
-    is_prime = True
+    if i == 1:
+        continue
     for j in range(2, i):
         if i % j == 0:
-            is_prime = False
             not_prime.append(i)
             break
     else:
